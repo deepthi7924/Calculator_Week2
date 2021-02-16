16d1a119a4f1ab2729df048b8b85e91681d3054f
deepthi <deepthi7924@gmail.com>
2021-02-16 22:50:57 +0530

    WEEK2 - CALCULATOR

---
+++ task2/src/main/java/epam/task2/Calci.java
@@ -0,0 +1,55 @@
+package epam.task2;
+import java.util.*;
+public class Calci {
+	public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+String[] arr=new String[3];
+Operations o=new Operations();
+System.out.println("enter 1 for add ,2 for sub, 3 for mul,4 for div ,5 for mod");
+int n=sc.nextInt();
+System.out.println("enter expression in format num1 operator number2");
+String s=sc.next()+sc.nextLine();
+//input for this program is in format a+b or a-b or a*b...
+switch (n) {
+case 1:
+{
+		arr=s.split("\\+");
+		int p=o.add(arr[0],arr[1]);
+		System.out.println(p);
+        break;
+}
+case 2:
+	 {
+		arr=s.split("\\-");
+		int p=o.sub(arr[0],arr[1]);
+		System.out.println(p);
+        break;
+	}
+case 3:
+	 {
+		arr=s.split("\\*");	
+		int p=o.mul(arr[0],arr[1]);
+		System.out.println(p);
+        break;
+	}
+case 4:
+	 {
+		arr=s.split("\\/");	
+		int p=o.div(arr[0],arr[1]);
+		System.out.println(p);
+        break;
+	}
+case 5:
+	{
+		arr=s.split("\\%");	
+		int p=o.mod(arr[0],arr[1]);
+		System.out.println(p);
+        break;
+	}
+
+}
+
+;sc.close();
+}
+}
